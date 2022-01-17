59fcc12649b36157992047f5fe681ca1a83db279
Niklas Coors <coors.niklas@gmail.com>
2022-01-17 16:54:14 +0100

    added routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+def create_content
+  paragraphs = []
+  paragraphs << Faker::Lorem.paragraph
+  paragraphs << "## #{Faker::Company.bs}"
+  paragraphs << Faker::Lorem.paragraphs(2)
+  paragraphs << "## #{Faker::Company.bs}"
+  paragraphs << Faker::Lorem.paragraphs(4)
+  paragraphs.flatten.join("\n\n")
+end
+
+10.times do
+  Article.create(
+    title: Faker::Company.bs,
+    content: create_content
+  )
+end
